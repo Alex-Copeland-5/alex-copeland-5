296167bd3e182df021317c0cc8e02dc60ae44505
Alex <alexcopeland2020@gmail.com>
2025-09-15 10:56:49 -0500

    Add files via upload

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -6,5 +6,5 @@ document.getElementById("randomBtn").addEventListener("click", function() {
   // Generate a random number between 1 and 100
   let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  document.getElementById("randomNumberDisplay").textContent = "Your random number is " + randomNumber; + "!";
+  document.getElementById("randomNumberDisplay").textContent = "Your random number is " + randomNumber + "!";
 });
